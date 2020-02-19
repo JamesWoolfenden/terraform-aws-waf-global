c17b226cca1b3b91c58536e60b4dc8a22e0ad4be
Hakan Ertas <hknerts@gmail.com>
2020-02-19 16:00:35 +0300

    Update outputs.tf
    adding web_acl_id output variable

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,7 @@ output "ip_whitelist" { value = aws_waf_web_acl.ip_whitelist }
 output "ip_match" {
   value = aws_waf_rule.ip_match
 }
+
+output "web_acl_id" {
+  value = aws_waf_web_acl.ip_whitelist.id
+}
